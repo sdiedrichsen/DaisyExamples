67d25c13028339bbb5ce1cad6eb782165504f9e3
sdiedrichsen <47812706+sdiedrichsen@users.noreply.github.com>
2021-03-29 20:32:50 +0200

    Add simple Controller

---
+++ patch/FDN/Controller.cpp
@@ -0,0 +1,59 @@
+/**
+ * @file    Controller.cpp
+ * @author Steffan Diedrichsen (sdiedrichsen@mac.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-03-29
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#include "Controller.h"
+#include <math.h>
+
+void Controller::Init
+( const char* pName, float min, float max, int steps, character response, float initialValue)
+{
+  _pName = pName;
+  _min = min;
+  _max = max;
+  _steps = steps;
+  _response = response;
+  _value = initialValue;
+  _pos   = 0;
+
+  _pos = roundf((_value - _min) * float(_steps) / (_max - _min));
+}
+
+void 
+Controller::Increment(int steps)
+{
+  ChangeValue(steps);
+}
+void 
+Controller::Decrement(int steps)
+{
+  ChangeValue(-steps);
+}
+void
+Controller::ChangeValue(int steps)
+{
+  if(_response == eLinear)
+  {
+    _pos += steps;
+    _pos = (_pos > _steps)? _steps : _pos;
+    _pos = (_pos <  0)?     0 : _pos;
+
+    _value = _min + (_max - _min) * float(_pos) /float(_steps);
+
+  }
+
+}
+
+
+float 
+Controller::Value()
+{
+  return _value;
+}
--- patch/FDN/Controller.h
+++ patch/FDN/Controller.h
@@ -11,4 +11,31 @@
 
 #pragma once
 
+enum character
+{
+    eLinear,
+    eLogarithmic
+};
 
+
+struct Controller
+{
+  const char* _pName;
+  float       _min;
+  float       _max;
+  int         _steps;
+  character   _response;
+
+  float       _value;
+  int         _pos; 
+
+  void Init(const char* pName, float min, float max, int steps, character response, float initialValue);
+
+  void Increment(int steps = 1);
+  void Decrement(int steps = 1);
+  float Value   ();
+
+  private:
+  void ChangeValue(int steps);
+
+};
