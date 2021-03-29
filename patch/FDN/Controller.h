335329b237cd0c0cf4618bebae5307ff0165c094
sdiedrichsen <47812706+sdiedrichsen@users.noreply.github.com>
2021-03-29 20:01:11 +0200

    Add file for controller class

---
+++ patch/FDN/Controller.h
@@ -0,0 +1,14 @@
+/**
+ * @file Controller.h
+ * @author Steffan Diedrichsen(sdiedrichsen@mac.com)
+ * @brief  simple class for control parameters
+ * @version 0.1
+ * @date 2021-03-29
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#pragma once
+
+
